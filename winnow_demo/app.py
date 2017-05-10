fe43a51785b797964641950222dd7ba5d5f730b1
Brian Schiller <bgschiller@gmail.com>
2017-05-10 18:03:13 -0500

    Add tomato

--- winnow_demo/app.py
+++ winnow_demo/app.py
@@ -53,7 +53,7 @@ PREDEFINED_FILTERS = [
                 },
             ],
         },
-        icon='<img src="/static/gluten-free-256.png">',
+        icon='/static/gluten-free-256.png',
     ),
     dict(
         name='Meat and potatoes',
@@ -80,7 +80,7 @@ PREDEFINED_FILTERS = [
                 },
             ],
         },
-        icon='<img src="/static/meat_and_potat.png">'
+        icon='/static/meat_and_potat.png'
     ),
     dict(
         name='Quick, use the tomatoes!',
@@ -99,33 +99,9 @@ PREDEFINED_FILTERS = [
                 },
             ],
         },
-        icon='<img src="/static/meat_and_potat.png">'
+        icon='/static/tomato.png'
     ),
 ]
-
-
-
-GLUTEN_FREE_RESULTS = [
-    {
-        'cook_time': '00:05:00',
-        'date_published': datetime.datetime(2009, 9, 6, 0, 0),
-        'description': 'A twist on the classic, I use crisp brown rice cereal slathered in a decadent peanut butter maple syrup sludge. This version of treats has bits of chopped, toasted pistachios throughout - also vegan (no marshmallows) and gluten-free.',
-        'id': 865,
-        'img_url': 'http://www.101cookbooks.com/mt-static/images/food/pb_rice_crispy_treats.jpg',
-        'name': 'Peanut Butter Krispy Treats',
-        'prep_time': '00:10:00',
-        'url': 'http://www.101cookbooks.com/archives/peanut-butter-krispy-treats-recipe.html',
-        'yield': ''},
-    {
-        'cook_time': None,
-        'date_published': datetime.datetime(2007, 10, 22, 0, 0),
-        'description': 'A grown-up twist on the figgy classic. This filling for this fig cookie recipe is dried figs that are marinated overnight in port and pomegranate juice and then pureed. They also happen to be gluten-free.',
-        'id': 957,
-        'img_url': 'http://www.101cookbooks.com/mt-static/images/food/figcookierecipe_07.jpg',
-        'name': 'Grown-up Fig Cookies',
-        'prep_time': None,
-        'url': 'http://www.101cookbooks.com/archives/grownup-fig-cookies-recipe.html',
-        'yield': ''}]
 
 
 def find_where(lst, **kwargs):
@@ -150,7 +126,7 @@ def index():
         filt = None
         results = None
         # Just for debugging
-        results = prepare_and_perform_query(PREDEFINED_FILTERS[0]['filt'])
+        results = prepare_and_perform_query(PREDEFINED_FILTERS[1]['filt'])
 
     results['params'] = json.dumps(results['params'], indent=4)
     return render_template(
